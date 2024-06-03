fd88b46e79e7cd63230f424223fc2817de0e1bce
Silvia2015 <silvitacabellocatapablo@gmail.com>
2024-06-04 11:09:17 +1200

    error de validacion listo

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore} from 'vuex';
-import {firebaseApp} from '@/config/firebase';
+import {firebaseApp} from '../config/firebase';
 import {getAuth,signInWithEmailAndPassword,onAuthStateChanged,signOut} from 'firebase/auth';
 
 const auth = getAuth(firebaseApp);
@@ -7,16 +7,31 @@ const auth = getAuth(firebaseApp);
 export default createStore({
     state() {
         return {
-
+            authUser:null,
+            errorMsg:''
         }
     },
     mutations: {
-
+        setAuthUser(state,user) {
+            state.authuser = user;
+        },
+       setErrorMsg(state,message) {
+        state.errorMsg = message
+       }
     },
     actions: {
-
+        login({commit},{email,password,router}) {
+         signInWithEmailAndPassword(auth,email,password).then(userCredential => {
+            commit('setAuthUser',userCredential.user);
+            // router.push({name:'home'})
+         })
+         .catch(error => {
+           // console.log(error);
+            commit('setErrorMsg', 'Credenciales incorrectas')
+         })
+        }
     },
     getters: {
-        
+
     }
 })
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -2,12 +2,20 @@
 import {useForm,useField} from 'vee-validate';
 import { loginSchema as validationSchema } from '@/validation/loginSchema';
 import { useRouter } from 'vue-router';
+import {useStore} from 'vuex';
+import {computed} from 'vue';
+
+const store = useStore();
 const { handleSubmit } = useForm({ validationSchema });
 const email = useField('email');
 const password = useField('password');
+
 const submit = handleSubmit(values => {
- console.log(values)
+// console.log(values);
+ store.dispatch('login',values)
 })
+
+const hasError = computed(() => store.state.errorMsg)
 </script>
 
 <template>
@@ -15,15 +23,22 @@ const submit = handleSubmit(values => {
         <div class="row">
             <div class="col-12 offset-sm-3 col-sm-6">
                 <h1 class="text-center">Inicio Sesion</h1>
+
                 <form class="p-4 shadow">
+                    <div v-if="hasError" class="alert alert-danger" role="alert">
+                    {{ store.state.errorMsg }}
+                    </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Correo Electronico</label>
                         <input type="email" class="form-control" id="email" v-model="email.value.value" required>
+                        <div v-if="email.errorMessage" id="emailHelp" class="text-danger form-text">{{ email.errorMessage.value }}</div>
+                     
                         
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Contraseña</label>
                         <input type="password" class="form-control" id="password" v-model="password.value.value" required>
+                        <div v-if="password.errorMessage" id="emailHelp" class="text-danger form-text">{{ password.errorMessage.value }}</div>
                     </div>
                     <button type="submit" class="btn btn-primary w-100" @click="submit">Ingresar</button>
                 </form>
