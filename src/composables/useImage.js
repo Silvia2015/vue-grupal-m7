5df89756dea1249920449ad04634b0f428e99e52
Silvia2015 <silvitacabellocatapablo@gmail.com>
2024-06-05 12:11:18 +1200

    agregar productos

---
+++ src/composables/useImage.js
@@ -0,0 +1,55 @@
+import { computed, ref } from 'vue';
+import { ref as storageRef, getDownloadURL } from 'firebase/storage'; 
+import { useFirebaseStorage, useStorageFile } from 'vuefire'; 
+import { uid } from 'uid'; 
+
+export default function useImage() {
+  const storage = useFirebaseStorage(); 
+
+  let storageRefPath;
+  const url = ref(null);
+  const uploadError = ref(null);
+  const uploadProgress = ref(0);
+
+  async function uploadImage(event) {
+    const data = event.target.files[0]; 
+    console.log('Select file:', data);
+
+    if (data) {
+      const fileExtension = data.name.split('.').pop(); 
+      storageRefPath = storageRef(storage, `/productos/${uid()}.${fileExtension}`); 
+      console.log(storageRefPath);
+
+      const {
+        upload, 
+        uploadError: ue,
+        uploadProgress: up
+      } = useStorageFile(storageRefPath);
+
+      try {
+        await upload(data);
+        const downloadUrl = await getDownloadURL(storageRefPath); 
+        console.log('Image uploaded successfully: ', downloadUrl);
+        url.value = downloadUrl;
+        uploadError.value = null;
+        uploadProgress.value = 100;
+      } catch (error) {
+        console.error('Error uploading image:', error);
+        uploadError.value = ue;
+        uploadProgress.value = up;
+      }
+    }
+  }
+
+  const image = computed(() => {
+    return url.value ? url.value : null;
+  });
+
+  return {
+    url, 
+    uploadImage,
+    image,
+    uploadError,
+    uploadProgress
+  };
+}
--- src/views/admin/NewProductView.vue
+++ src/views/admin/NewProductView.vue
@@ -2,12 +2,70 @@
 </script>
 
 <template>
-<div class="container">
-    <h2>Nuevo Producto</h2>
-</div>
-
-</template>
+    <div class="container">
+      <div class="row">
+        <div class="offset-3 col-6">
+          <h2>Nuevo Producto</h2>
+  
+          <form>
+            <div class="mb-3">
+              <label for="nombre" class="form-label">Nombre</label>
+              <input type="text" class="form-control" id="nombre"  required>
+            </div>
+            <div class="mb-3">
+              <label for="" class="form-label">Imagen</label>
+              <input type="file" class="form-control" id="imagen" required>
+            </div>
+  
+           
+  
+  
+            <div class="mb-3">
+              <label for="precio" class="form-label">Precio</label>
+              <input type="number" class="form-control" id="precio" required>
+            </div>
+            <div class="mb-3">
+              <label for="stock" class="form-label">Stock</label>
+              <input type="number" class="form-control" id="stock"  required>
+            </div>
+         
+              <button type="submit" class="btn btn-dark" >Guardar <i class="fa-solid fa-floppy-disk"></i></button>
+           
+  
+          </form>
+        </div>
+      </div>
+  
+    </div>
+  </template>
 
 <style>
 
 </style>
+<!--
+v-model="nombre.value.value"
+@change="uploadImage" 
+ <div class="mb-3" v-if="image">
+              <img :src="image" class="img-thumbnail rounded mx-auto d-block" alt="imagen producto" width="150">
+            </div>
+ v-model="precio.value.value"
+ v-model="stock.value.value"
+  <label for="caracteristicas" class="form-label">Características</label>
+              <div v-for="(caracteristica, index) in caracteristicas" :key="index" class="d-flex mb-2">
+                <input type="text" class="form-control me-2" v-model="caracteristica.key" placeholder="Característica"
+                  required>
+                <input type="text" class="form-control me-2" v-model="caracteristica.value" placeholder="Valor" required>
+                <button type="button" class="btn btn-danger" @click="removeCaracteristica(index)"><i
+                    class="fa-solid fa-trash"></i></button>
+                     </div>
+                        <div class="mb-3">
+                            @click="submit"
+             
+             
+  
+            </div>
+            <div class="d-grid gap-2">
+              <button type="button" class="btn btn-dark" @click="addCaracteristica">Agregar Caracteristica <i
+                  class="fa-solid fa-plus"></i></button>
+
+-->
